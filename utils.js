712126e67fb03d958a505546cd6c41a7c8dce0e6
Lily Maksutova <lily.maksutova@mail.ru>
2024-09-15 01:02:55 +0300

    set random order

--- utils.js
+++ utils.js
@@ -1,22 +1,40 @@
 const questions = require('./questions.json');
 const { Random } = require('random-js');
 
+const usedQuestions = {};
+
 const getRandomQuestion = (topic) => {
   const random = new Random();
   let questionTopic = topic.toLowerCase();
+
   if (topic === 'случайный вопрос') {
     questionTopic =
       Object.keys(questions)[
         random.integer(0, Object.keys(questions).length - 1)
       ];
   }
-  const randomQuestionIndex = random.integer(
-    0,
-    questions[questionTopic].length - 1
+
+  if (!usedQuestions[questionTopic]) {
+    usedQuestions[questionTopic] = [];
+  }
+
+  const availableQuestions = questions[questionTopic].filter(
+    (q) => !usedQuestions[questionTopic].includes(q.id)
   );
 
+  if (availableQuestions.length === 0) {
+    usedQuestions[questionTopic] = [];
+    return getRandomQuestion(topic);
+  }
+
+  const randomQuestionIndex = random.integer(0, availableQuestions.length - 1);
+
+  const selectedQuestion = availableQuestions[randomQuestionIndex];
+
+  usedQuestions[questionTopic].push(selectedQuestion.id);
+
   return {
-    question: questions[questionTopic][randomQuestionIndex],
+    question: selectedQuestion,
     questionTopic,
   };
 };
